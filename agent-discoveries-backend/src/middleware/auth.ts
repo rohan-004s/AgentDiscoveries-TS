e6410ac54de295141e1b7c6ac10cce0bf90f49d8
Ayxh <vulpix@ayxh.dev>
2022-10-25 07:18:25 +0100

    Remove console log

--- agent-discoveries-backend/src/middleware/auth.ts
+++ agent-discoveries-backend/src/middleware/auth.ts
@@ -32,8 +32,6 @@ export default function createAuthMiddleware(knex: Knex) {
     next: NextFunction,
   ) {
     try {
-      console.log(JSON.stringify(req.session.user))
-
       const isAdmin = await knex
         .select('admin')
         .from<User>('users')
