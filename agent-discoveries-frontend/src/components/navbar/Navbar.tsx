bb2adf4c3389a7bf00474bbc053d9f073254fe27
Matthew Crossley <matthew.crossley@bp.com>
2022-10-26 13:54:11 +0100

    Add navigation bar react component

---
+++ agent-discoveries-frontend/src/components/navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Navbar: React.FC = () => {
+
+    return (
+        <>
+            <nav id="navbar" className="navbar" data-testid="nav-bar">
+                <h1 className="title" id="navbar-title">Agent Discoveries</h1>
+                <div className="items" id="navbar-items" data-testid="navbar-items">
+                    <a href="/home" data-testid="navbar-item">Homepage</a>
+                    <a href="/profile" data-testid="navbar-item">Your Profile</a>
+                    <a href="/about" data-testid="navbar-item">About</a>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default Navbar;
