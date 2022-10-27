8b6d1736c0e9470121676848fff17fa672badfe8
Matthew Crossley <matthew.crossley@bp.com>
2022-10-27 11:49:19 +0100

    Fix tests again

--- agent-discoveries-frontend/src/components/navbar/Navbar.tsx
+++ agent-discoveries-frontend/src/components/navbar/Navbar.tsx
@@ -1,19 +1,25 @@
-import "./Navbar.styles.css"
+import './Navbar.styles.css'
 
 const Navbar = () => {
+  return (
+    <nav id="navbar" className="navbar" data-testid="navbar">
+      <h1 className="title" id="navbar-title">
+        Agent Discoveries
+      </h1>
+      <div className="items" id="navbar-items" data-testid="navbar-items">
+        <a href="/home" data-testid="navbar-item">
+          Homepage
+        </a>
+        <a href="/about" data-testid="navbar-item">
+          About
+        </a>
+        <a href="/profile" data-testid="navbar-item">
+          Your Profile
+        </a>
+        <img src="/profile.png" />
+      </div>
+    </nav>
+  )
+}
 
-    return (
-        <>
-            <nav id="navbar" className="navbar" data-testid="nav-bar">
-                <h1 className="title" id="navbar-title">Agent Discoveries</h1>
-                <div className="items" id="navbar-items" data-testid="navbar-items">
-                    <a href="/home" data-testid="navbar-item">Homepage</a>
-                    <a href="/about" data-testid="navbar-item">About</a>
-                    <a href="/profile" data-testid="navbar-item">Your Profile</a>
-                </div>
-            </nav>
-        </>
-    );
-};
-
-export default Navbar;
+export default Navbar
