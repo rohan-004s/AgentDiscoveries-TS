dca2ee457aaaa129bc5fffb69c400aa276668d58
MatthewCrossley <114577813+MatthewCrossley@users.noreply.github.com>
2022-10-28 10:50:35 +0100

    Update navbar design (#6)
    * Add basic ability to render profile pictures
    
    * Add navbar search bar and profile picture

--- agent-discoveries-frontend/src/components/navbar/Navbar.test.tsx
+++ agent-discoveries-frontend/src/components/navbar/Navbar.test.tsx
@@ -8,18 +8,18 @@ afterEach(() => {
 
 describe('Navbar', () => {
   it('Matches the snapshot', () => {
-    const navbar = render(<Navbar />)
+    const navbar = render(<Navbar imageUrl={''} />)
     expect(navbar.asFragment()).toMatchSnapshot()
   })
 
   it('Must render on page', () => {
-    render(<Navbar />)
+    render(<Navbar imageUrl={''} />)
     const navbar = screen.getByTestId('navbar')
     expect(navbar).toBeInTheDocument()
   })
 
   test('Renders with all items', () => {
-    render(<Navbar />)
+    render(<Navbar imageUrl={''} />)
     const navbarItems = screen.getAllByTestId('navbar-item')
     expect(navbarItems).toHaveLength(3)
   })
