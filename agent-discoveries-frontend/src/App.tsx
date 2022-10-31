96bb1a23cb1f85bae0dfd258bb5382fbda3ab5aa
Jenath <jenathkanagesan@gmail.com>
2022-10-31 15:18:02 +0000

    profile page basic

--- agent-discoveries-frontend/src/App.tsx
+++ agent-discoveries-frontend/src/App.tsx
@@ -2,20 +2,16 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Login from './Views/Login/Login'
 import HomePage from './Views/Home/HomePage'
+import ProfilePage from './Views/ProfilePage/ProfilePage'
+
 import { useState } from 'react'
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(true)
 
   return (
     <>
-        {
-          isLoggedIn ? (
-          <HomePage />
-        ) : (
-          <Login setIsLoggedIn={setIsLoggedIn} />
-        )
-      }
+      {isLoggedIn ? <HomePage /> : <Login setIsLoggedIn={setIsLoggedIn} />}
     </>
   )
 }
--- agent-discoveries-frontend/src/Views/ProfilePage/ProfilePage.tsx
+++ agent-discoveries-frontend/src/Views/ProfilePage/ProfilePage.tsx
@@ -1,11 +1,62 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-=======
 import React from 'react'
+import './ProfilePage.css'
+import Title from '../../Components/Title/title'
+import MessageBoard from '../../Components/MessageBoard/messageBoard'
+import AgentsList from '../../Components/AgentsList/agentsList'
+import PostMessage from '../../Components/PostMessage/postMessage'
 
-const ProfilePage = () => {
-  return <></>
+function Header() {
+  return (
+    <header className="banner">
+      <button>Edit Profile</button>
+      <img src="" alt="" />
+      <p>Location</p>
+      <p>Time</p>
+    </header>
+  )
 }
 
-export default ProfilePage
->>>>>>> d4d6a6989e93a82509313fc4848199525ac846da
+function AccountInfo() {
+  return (
+    <section className="column column1">
+      <h2>Account Information</h2>
+      <p>Username:</p>
+      <p>Email:</p>
+      <p>Base Location:</p>
+      <p>Contact Details:</p>
+      <p>Password:</p>
+    </section>
+  )
+}
+
+function JobProfile() {
+  return (
+    <section className="column column2">
+      <h2>Job Profile</h2>
+      <p>Job title:</p>
+      <p>Manager:</p>
+      <p>Biography:</p>
+      <p>Years of service:</p>
+    </section>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <button>Save Changes</button>
+      <button>Delete Account</button>
+    </footer>
+  )
+}
+
+export default function ProfilePage() {
+  return (
+    <section className="page">
+      <Header />
+      <AccountInfo />
+      <JobProfile />
+      <Footer />
+    </section>
+  )
+}
